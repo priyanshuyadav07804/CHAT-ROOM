f2dd98c8f49eabaf76c655f1722cbe9d35e7ffb7
priyanshu yadav <80442935+priyanshuyadav07804@users.noreply.github.com>
2023-06-29 02:41:58 +0530

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-import { io } from 'socket.io-client';
+const socketio = require('socket.io-client');
 const chatForm = document.getElementById('chat-form')
 const chatMessages = document.querySelector('.chat-messages')
 const roomName = document.getElementById('room-name')
